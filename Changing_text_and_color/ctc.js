f2e96b65a6c46f0747ff1339e21d0a276ebb885c
sakshi978 <57284653+sakshi978@users.noreply.github.com>
2020-05-28 10:10:52 +0530

    Add files via upload

---
+++ Changing_text_and_color/ctc.js
@@ -0,0 +1,14 @@
+function changecolor() {
+    var dd1 = document.getElementById("d1");
+    var dd2 = document.getElementById("d2");
+
+    dd1.className='blackback';
+    dd2.className='yellowback';
+}
+function changetext() {
+    var dt1 = document.getElementById("d1");
+    var dt2 = document.getElementById("d2");
+
+    dt1.innerHTML="GoodBye";
+    dt2.innerHTML="Everyone";
+}
